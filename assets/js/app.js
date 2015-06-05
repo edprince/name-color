e3d6892cd6102e21ce6659ae5c8c686c41b25a9a
ed <edward_prince@hotmail.com>
2015-06-05 20:50:51 +0100

    color variation

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,33 +5,25 @@ function generateColor() {
   var convert = [];
   if (validateString(name)) {
     //do the colour generation
-    var r;
-    var g;
-    var b;
-   
-  var number;
-    for (var i = 0; i < name.length; i++) {
-      if ((name.charCodeAt(i) % 2) === 0) {
-        number = Math.floor(name.charCodeAt(i) / 10);
-      } else {
-        number = name.charCodeAt(i);
-      }
-      convert[i] = number; 
-      console.log("Letter conversion: " + convert[i]);
+    var r = name.charCodeAt(0);
+    var g = name.charCodeAt(name.length - 1);
+    var b = name.charCodeAt(Math.floor(name.length / 2));
+    var number;
       
-    }
+      
     //An array of codes
-    r = convert[0];
-    g = convert[name.length - 1];
-    b = convert[Math.floor(name.length / 2)];
-
-    console.log(r);
-    console.log(g);
-    console.log(b);
     
-    var color = "rgba(" + r + ", " + g + ", " + b + ", 1)";
+    var x = Math.floor(name.charCodeAt(0) / 2);
+    console.log(x);  
+    var y = x - 30;
+    var average = Math.floor(((r + g + b) / 2) + x * y);
+    
+    console.log(average);
+    
+    var color = "hsl(" + average + ", 60%, 50%)";
 
     document.getElementById('display').style.background = color;
+    document.getElementById('hsl').innerHTML = "HSL = (" + average + ", 60%, 50%)";
   } else {
   alert("Please enter a valid string");
   }
